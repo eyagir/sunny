4b318e9993b0a46ce22f62570d01841bde0af67a
Grant <grant4e@live.unc.edu>
2019-10-13 01:34:05 -0400

    switching to grant branch

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import io.coursicle_scraper
+import io.course_block
+
+def __main__():
+    return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if('__name__' == '__main__'):
+    main()
