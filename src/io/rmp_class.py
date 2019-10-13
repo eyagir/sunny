7e1608fc9e4ea1a47153af32a2fefeea36f908a4
eyagir <eyup@live.unc.edu>
2019-10-12 23:57:18 -0400

    bruh

--- src/io/rmp_class.py
+++ src/io/rmp_class.py
@@ -16,12 +16,30 @@ url = "https://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=U
 session = dryscrape.Session()
 session.set_attribute('auto_load_images', False)
 session.visit(url)
+# <div class="content" onclick="javascript:mtvn.btg.Controller.sendLinkEvent({ linkName:'PROFMIDPANE:LoadMore', linkType:'o' } );">Load More</div>
+overlapping_element = session.at_xpath("//*[@id='myProfprogressbtnwrap']")
+load_more = overlapping_element.exec_sript('node.parentElement.removeChild(node)')
+for i in range(5):
+    load_more.click()
+    print("Fuck")
 
 page = session.body()
+#print(page)
 parsed_html = BeautifulSoup(page, 'lxml')
 
-print(parsed_html)
-list_of_profs = parsed_html.find('class', attrs = {'class':'result-list'})
-print(list_of_profs)
-list_of_profs = parsed_html.find_all("li", attrs = {id: re.compile('my-professor*')})
-print(list_of_profs)
+
+#print(parsed_html)
+list_of_profs = parsed_html.find('div', attrs = {'id':'body'})
+list_of_profs = list_of_profs.find('div', attrs = {'id':'mainContent'})
+list_of_profs = list_of_profs.find('div', attrs = {'class':'left-panel'})
+list_of_profs = list_of_profs.find('div', attrs = {'class':'side-panel'})
+list_of_profs = list_of_profs.find('div', attrs = {'class':'result-list'})
+#print(list_of_profs)
+list_of_profs = list_of_profs.find_all('li', attrs = {'id': re.compile('my-professor*')})
+y = 0
+for x in list_of_profs:
+    name = x.find('span', attrs = {'class':'name'}).text
+    rating = x.find('span', attrs = {'class':'rating'}).text
+    print(str(name) , str(rating))
+    y += 1
+print(y)
